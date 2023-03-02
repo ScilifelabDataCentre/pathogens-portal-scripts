41f7d4d0104b2cac75dc623a80f0c40c127217c8
LianeHughes <68756672+LianeHughes@users.noreply.github.com>
2023-03-02 07:45:05 +0100

    add info about date format

--- Available_data/pmc_pub_seeker.py
+++ Available_data/pmc_pub_seeker.py
@@ -4,7 +4,9 @@ import json
 import requests
 
 # Method to build url that will be used to make the API request
-def build_pmc_query_url(query_string, result_type="core", page_size=100, return_format="json"):
+def build_pmc_query_url(
+    query_string, result_type="core", page_size=100, return_format="json"
+):
     pmc_api_base = "https://www.ebi.ac.uk/europepmc/webservices/rest/search?"
     query_url = pmc_api_base + "query={}".format(query_string)
     query_url += "&resultType={}".format(result_type)
@@ -12,14 +14,21 @@ def build_pmc_query_url(query_string, result_type="core", page_size=100, return_
     query_url += "&format={}".format(return_format)
     return query_url
 
+
 # A generator that makes the API call and yields the publication
 def pmc_get_publications(query_url):
     response = requests.get(query_url)
     response_dict = response.json()
-    for result in response_dict['resultList']['result']:
-        yield [result.get("id"), result.get("source"), result.get("title"), result.get("hasData")]
-    if 'nextPageUrl' in response_dict.keys():
-        yield from pmc_get_publications(response_dict['nextPageUrl'])
+    for result in response_dict["resultList"]["result"]:
+        yield [
+            result.get("id"),
+            result.get("source"),
+            result.get("title"),
+            result.get("hasData"),
+        ]
+    if "nextPageUrl" in response_dict.keys():
+        yield from pmc_get_publications(response_dict["nextPageUrl"])
+
 
 # Truncate long title to fit in the table
 def fit_title(title):
@@ -27,36 +36,56 @@ def fit_title(title):
         title = title[:117] + "..."
     return title
 
+
 def pprint(dobj):
     print(json.dumps(dobj, indent=4))
 
+
+# add date rangein format e.g. 2023-02-01 TO 2023-02-28 in the square brackets
 main_query_string = '("SARS-CoV-2" OR "COVID-19" OR "Covid-19") AND AFF:"Sweden" AND CREATION_DATE:[<start> TO <end>]'
 
-words_of_interest = {'Figshare' : 'figshare*', 'Zenodo' : 'zenodo*', 'Github' : 'github*', 'Dryad': 'dryad',
-                     'Gene Expression Omnibus' : 'gse*', 'Protein Data Bank' : 'PDB*', 'Proteome Xchange' : '(PXD* OR ProteomeXchange)',
-                     'SASBDB' : 'SASD*', 'Electron Microscopy DB' : '(EMD AND NOT serono)', 'ENA' : '(PRJE* OR PRJD* OR PRJN*)',
-                     'Open Science Framework' : '("Open Science Framework" OR osf*)', 'Experimental Factor Ontology' : 'EFO'}
+words_of_interest = {
+    "Figshare": "figshare*",
+    "Zenodo": "zenodo*",
+    "Github": "github*",
+    "Dryad": "dryad",
+    "Gene Expression Omnibus": "gse*",
+    "Protein Data Bank": "PDB*",
+    "Proteome Xchange": "(PXD* OR ProteomeXchange)",
+    "SASBDB": "SASD*",
+    "Electron Microscopy DB": "(EMD AND NOT serono)",
+    "ENA": "(PRJE* OR PRJD* OR PRJN*)",
+    "Open Science Framework": '("Open Science Framework" OR osf*)',
+    "Experimental Factor Ontology": "EFO",
+}
 
-article_web_base = 'https://europepmc.org/article'
+article_web_base = "https://europepmc.org/article"
 pub_collection = {}
-pub_summary = {'total': 0, 'data_y': 0, 'data_n': 0, 'db_total': 0}
+pub_summary = {"total": 0, "data_y": 0, "data_n": 0, "db_total": 0}
 
 # query again with words of interest and populate it in publications info
 for db, search_word in words_of_interest.items():
-    wqurl = build_pmc_query_url(query_string="{} AND {}".format(main_query_string, search_word))
+    wqurl = build_pmc_query_url(
+        query_string="{} AND {}".format(main_query_string, search_word)
+    )
     for pub in pmc_get_publications(wqurl):
         if pub[0] not in pub_collection:
-            #print("Strange!! Article '{}' has word of interest '{}' but not in main search".format((article_web_base + '/' + pub[1] + '/' + pub[0]), db))
-            pub_collection[pub[0]] = ((article_web_base + '/' + pub[1] + '/' + pub[0]), pub[2], pub[3], [])
-            pub_summary['total'] += 1
-            pub_summary['data_' + pub[3].lower()] += 1
+            # print("Strange!! Article '{}' has word of interest '{}' but not in main search".format((article_web_base + '/' + pub[1] + '/' + pub[0]), db))
+            pub_collection[pub[0]] = (
+                (article_web_base + "/" + pub[1] + "/" + pub[0]),
+                pub[2],
+                pub[3],
+                [],
+            )
+            pub_summary["total"] += 1
+            pub_summary["data_" + pub[3].lower()] += 1
         pub_collection[pub[0]][-1].append(db)
     print("Done checking for DB {}".format(db))
 
 # write the output file
 with open("publication_list.html", "w") as outfile:
     outfile.write(
-    """
+        """
         <!DOCTYPE html>
         <html>
         <head>
@@ -120,21 +149,30 @@ with open("publication_list.html", "w") as outfile:
         <div id='TableContainer'>
         <table class='sortable'>
         <tr><th>Read</th><th>Title</th><th>Has Data</th><th>Mentioned DB of interest</th></tr>
-    """.format(pub_summary['total'], pub_summary['data_y'], pub_summary['data_n'])
+    """.format(
+            pub_summary["total"], pub_summary["data_y"], pub_summary["data_n"]
+        )
     )
     for pid, pub_info in pub_collection.items():
         outfile.write(
-        """
+            """
             <tr id='{}'>
                 <td><input type='checkbox' id='{}'></td>
                 <td><a href='{}'>{}</a></td>
                 <td>{}</td>
                 <td>{}</td>
             </tr>
-        """.format(pid, pid, pub_info[0], fit_title(pub_info[1]), pub_info[2], ", ".join(pub_info[3]))
+        """.format(
+                pid,
+                pid,
+                pub_info[0],
+                fit_title(pub_info[1]),
+                pub_info[2],
+                ", ".join(pub_info[3]),
+            )
         )
     outfile.write(
-    """
+        """
         </table>
         </div>
         <script>
@@ -171,4 +209,4 @@ with open("publication_list.html", "w") as outfile:
         </body>
         </html>
     """
-    )    
+    )
